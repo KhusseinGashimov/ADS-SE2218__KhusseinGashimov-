88d93d052f74bf41b48684ee96b5294aac841e5e
Gahoo <rmit25@mail.ru>
2023-04-05 08:26:55 +0600

    find Fibonacci Problem 5

--- Main.java
+++ Main.java
@@ -35,9 +35,14 @@ import java.util.Scanner;
             else
                 return n*factorial(n-1);
         }
+        static int fib(int n){
+            if(n<=1)
+                return n;
+            return fib(n-1)+fib(n-2);
+        }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n=sc.nextInt();
-            System.out.println(factorial(n));
+            System.out.println(fib(n));
         }
     }
