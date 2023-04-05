5cbb31547b115c3e54a9bda973a8ccb4a101d244
Gahoo <rmit25@mail.ru>
2023-04-05 08:24:18 +0600

    find Factorial Problem 4

--- Main.java
+++ Main.java
@@ -29,9 +29,15 @@ import java.util.Scanner;
                 return isPrimeRecursiveHelper(n, i+1);
 
         }
+        static int factorial(int n){
+            if (n==1)
+                return 1;
+            else
+                return n*factorial(n-1);
+        }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n=sc.nextInt();
-            System.out.println(isPrimeRecursive(n));
+            System.out.println(factorial(n));
         }
     }
