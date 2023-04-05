6412dc7601e6091642beb00c6e54ed0877cdd517
Gahoo <rmit25@mail.ru>
2023-04-05 08:17:43 +0600

    find Average In Array Problem 2

--- Main.java
+++ Main.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
             else
                 return Math.min(nums[n-1], minimumInArray(n-1, nums));
         }
+        static double average(int n, int[] nums){
+            if (n == 0) {
+                return 0;
+            } else {
+                return (nums[n-1] + (n-1)*average(n-1, nums))/n;
+            }
+        }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n=sc.nextInt();
@@ -13,6 +20,6 @@ import java.util.Scanner;
             for (int i = 0; i < n; i++) {
                 nums[i]=sc.nextInt();
             }
-            System.out.println(minimumInArray(n,nums));
+            System.out.println(average(n,nums));
         }
     }
