48b4da5a033d5f803c8df27731720c4cb34f7e31
Gahoo <rmit25@mail.ru>
2023-04-05 08:29:25 +0600

    find n to the power a Problem 6

--- Main.java
+++ Main.java
@@ -40,9 +40,17 @@ import java.util.Scanner;
                 return n;
             return fib(n-1)+fib(n-2);
         }
+        static int twice(int a,int b){
+            int answer = 1;
+            if (b==0)
+                return answer;
+            else
+                return answer=a*twice(2,b-1);
+        }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n=sc.nextInt();
-            System.out.println(fib(n));
+            int a=sc.nextInt();
+            System.out.println(twice(n,a));
         }
     }
